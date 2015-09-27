606a2c70df9529783b0cae01549605ec862d1c8d
Horrgs <horrgs34@gmail.com>
2015-09-27 04:41:22 -0400

    Tweaks and doo-dads

---
+++ src/org/horrgs/twitterbot/io/WordInfo.java
@@ -0,0 +1,7 @@
+package org.horrgs.twitterbot.io;
+
+/**
+ * Created by Horrgs on 9/27/2015.
+ */
+public interface WordInfo {
+}
--- src/org/horrgs/twitterbot/util/Words.java
+++ src/org/horrgs/twitterbot/util/Words.java
@@ -38,21 +38,25 @@ public class Words {
     public String defineWord(String word) {
         String[] k = {"all", ""};
         try {
-            //TODO: get just the definition from it.
-
+            Site site = new Site("http://api.wordnik.com:80/v4/word.json/" + word
+                    + "/definitions?limit=1&includeRelated=true&sourceDictionaries=all&useCanonical=false" +
+                    "&includeTags=false&api_key=" + "a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5");
+            site.startJson(true, null, null);
+            return site.get(site.getJsonArray().getJSONObject(0), "text");
         } catch (JSONException ex) {
             ex.printStackTrace();
         }
         return null;
     }
 
-    public String[] defineWord(String word, int limit, boolean includeRelated, String[] sourceDictionaries, boolean useCanonical) {
+    public String[] getWordInfo(String word, int limit, boolean includeRelated, String[] sourceDictionaries, boolean useCanonical) {
         try {
             //TODO: sourceDictionaries.
             Site site = new Site("http://api.wordnik.com:80/v4/word.json/" + word
                     + "/definitions?limit=" + limit + "&includeRelated=" + includeRelated + "&sourceDictionaries=all&useCanonical=" +
                     useCanonical + "&includeTags=false&api_key=" + "a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5");
             site.startJson(true, null, null);
+            //WIP. This is wrong.
             return site.get(site.getJsonArray().getJSONObject(0), "text");
         } catch (JSONException ex) {
             ex.printStackTrace();
