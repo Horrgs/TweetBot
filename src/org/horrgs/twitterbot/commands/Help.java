36d285981fc1a075b781351ead86d7290bc87789
Horrgs <horrgs34@gmail.com>
2015-10-18 02:03:21 -0400

    Help command es done.

--- src/org/horrgs/twitterbot/commands/Help.java
+++ src/org/horrgs/twitterbot/commands/Help.java
@@ -12,13 +12,32 @@ import twitter4j.User;
 public class Help implements SubCommand {
     @Override
     public void onCommand(Status status, String[] args) {
+        StatusUpdate statusUpdate = new StatusUpdate("");
         long r1 = status.getId();
-        StatusUpdate statusUpdate = new StatusUpdate("@" + status.getUser().getScreenName() + " here are a list of commands: http://pastebin.com/5y1MtE6f");
-        statusUpdate.setInReplyToStatusId(r1);
-        try {
-            HorrgsTwitter.twitter.updateStatus(statusUpdate);
-        } catch (TwitterException ex) {
-            ex.printStackTrace();
+        if(args.length == 1) {
+            statusUpdate = new StatusUpdate("@" + status.getUser().getScreenName() + " here are a list of commands: http://pastebin.com/5y1MtE6f");
+            statusUpdate.setInReplyToStatusId(r1);
+            try {
+                HorrgsTwitter.twitter.updateStatus(statusUpdate);
+            } catch (TwitterException ex) {
+                ex.printStackTrace();
+            }
+        } else if(args.length == 2) {
+            String command = args[1];
+            try {
+                Class<?> myClass = Class.forName("org.horrgs.twiterbot.commands." + command);
+                Object a = myClass.newInstance();
+                if (a instanceof SubCommand) {
+                    SubCommand subCommand = (SubCommand ) a;
+                    statusUpdate = new StatusUpdate("@" + status.getUser().getScreenName() + " " + command + " - " + subCommand.getHelp());
+                    statusUpdate.setInReplyToStatusId(r1);
+                    HorrgsTwitter.twitter.updateStatus(statusUpdate);
+                }
+            } catch (ClassNotFoundException | IllegalAccessException | InstantiationException | TwitterException ex) {
+                ex.printStackTrace();
+            }
+        } else {
+            //TODO: invalid syntax.
         }
     }
 
